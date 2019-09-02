ff1c25e13ced76bf9d516653073a1c7eea9a8a79
Naoki Kuromiya <skn047@gmail.com>
2019-09-03 08:25:53 +0900

    revised 1.py for testing overlay

--- Question_01_10/my_answer/1.py
+++ Question_01_10/my_answer/1.py
@@ -11,9 +11,28 @@ def BGR2RGB(img):
 
     return img
 
+def Red(img):
+    b = img[:, :, 0].copy() 
+    g = img[:, :, 1].copy()
+    r = img[:, :, 2].copy()
+
+    out = cv2.imread('out.jpg')
+    
+    # img[:, :, 0] = 0
+    # img[:, :, 1] = 0
+    # img[:, :, 2] = r
+
+    img[img[:, :, 2] > 0][2] = out[img[:, :, 2] > 0][2]
+
+    print(out.shape)
+    a = img[:, :, 2] > 10
+    b = img > 10
+
+    return img
+    
 img_BGR = cv2.imread('imori.jpg')
 img_RGB = BGR2RGB(img_BGR)
+img_red = Red(img_BGR)
 
-cv2.imshow('result', img_RGB)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow('result', img_RGB)
+cv2.imwrite('red.png', img_red)
